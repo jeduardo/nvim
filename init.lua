0956c037d7968301023685b56362b308cc399953
J. Eduardo <j.eduardo@gmail.com>
2024-10-16 20:47:51 +0200

    Added transparency

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- From https://blog.chaitanyashahare.com/posts/how-to-make-nvim-backround-transparent/
+vim.cmd([[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]])
