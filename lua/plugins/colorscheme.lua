071e208da92568884411e0d56325e455c1aeb534
J. Eduardo <j.eduardo@gmail.com>
2024-10-16 20:31:09 +0200

    Fixed theme selection based on system preference for macOS

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,22 @@
 local function is_dark()
-  -- Change the background based on system preference
-  if vim.fn.has('mac') == 1 then
+  if vim.fn.has("mac") == 1 then
     -- macOS-specific check
     local handle =
       io.popen('osascript -e "tell application \\"System Events\\" to tell appearance preferences to return dark mode"')
-    local result = handle:read('*a')
+    local result = handle:read("*a")
     handle:close()
-    return result:match('true')
+    return result:match("true")
   end
   return true
+end
+
+local function select_theme(light, dark)
+  if is_dark() then
+    vim.o.background = "dark"
+    return dark
+  end
+  vim.o.background = "light"
+  return light
 end
 
 return {
@@ -20,14 +28,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = function()
-        if is_dark() then
-          vim.o.background = 'dark'
-          return 'nord'
-        end
-        vim.o.background = 'light'
-        return 'papercolor'
-      end,
+      colorscheme = select_theme("papercolor", "nord"),
     },
   },
 }
