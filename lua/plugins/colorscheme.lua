b25550c67a9a699d4903b0886ed419a5357f7900
J. Eduardo <j.eduardo@gmail.com>
2024-10-16 14:48:49 +0200

    Dynamic color change

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,33 @@
-return {
-  -- add gruvbox
-  { "nordtheme/vim" },
+local function is_dark()
+  -- Change the background based on system preference
+  if vim.fn.has('mac') == 1 then
+    -- macOS-specific check
+    local handle =
+      io.popen('osascript -e "tell application \\"System Events\\" to tell appearance preferences to return dark mode"')
+    local result = handle:read('*a')
+    handle:close()
+    return result:match('true')
+  end
+  return true
+end
 
-  -- Configure LazyVim to load gruvbox
+return {
+  -- Add themes
+  { "nordtheme/vim" },
+  { "NLKNguyen/papercolor-theme" },
+
+  -- Configure LazyVim to dynamically select the colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nord",
+      colorscheme = function()
+        if is_dark() then
+          vim.o.background = 'dark'
+          return 'nord'
+        end
+        vim.o.background = 'light'
+        return 'papercolor'
+      end,
     },
-  }
+  },
 }
