f2c660ae1832ec93036a0a3cd7bc675ead57211c
J. Eduardo <j.eduardo@gmail.com>
2024-11-01 09:26:33 +0100

    Removed buggy statement and added defensive coding

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,25 @@
+local BUF_SIZE = 4096 -- do not read more than this
+
 local function is_dark()
   if vim.fn.has("mac") == 1 then
     -- macOS-specific check
     local handle =
       io.popen('osascript -e "tell application \\"System Events\\" to tell appearance preferences to return dark mode"')
-    local result = handle:read("*a")
-    handle:close()
-    vim.api.nvim_err_writeln(vim.fn)
-    return result and result:match("true") and true
+    if handle then
+      local result = handle:read(BUF_SIZE)
+      handle:close()
+      return result and result:match("true") and true
+    end
   end
   if vim.fn.has("linux") == 1 then
     local handle = io.popen("gsettings get org.gnome.desktop.interface color-scheme")
-    local result = handle:read("*a")
-    handle:close()
-    return result and result:match("dark") or true
+    if handle then
+      local result = handle:read(BUF_SIZE)
+      handle:close()
+      return result and result:match("dark") or true
+    end
   end
+  -- Default to returning true, assuming that a light background is an exception
   return true
 end
 
