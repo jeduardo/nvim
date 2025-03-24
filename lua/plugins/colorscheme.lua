d9054e6356d973965a31632add0a7c7433e3c1e4
J. Eduardo <j.eduardo@gmail.com>
2025-03-24 16:21:22 +0100

    Fixed logic to detect dark theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,22 +1,30 @@
 local BUF_SIZE = 4096 -- do not read more than this
 
+local function isPatternMatch(str, pattern)
+  if str == nil or pattern == nil then
+    return false
+  end
+  return str:match(pattern) ~= nil
+end
+
 local function is_dark()
-  if vim.fn.has("mac") == 1 then
+  local osname = vim.loop.os_uname().sysname
+  if osname:match("Mac") then
     -- macOS-specific check
     local handle =
       io.popen('osascript -e "tell application \\"System Events\\" to tell appearance preferences to return dark mode"')
     if handle then
       local result = handle:read(BUF_SIZE)
       handle:close()
-      return result and result:match("true") and true
+      return isPatternMatch(result, "true")
     end
   end
-  if vim.fn.has("linux") == 1 then
+  if osname:match("Linux") then
     local handle = io.popen("gsettings get org.gnome.desktop.interface color-scheme")
     if handle then
       local result = handle:read(BUF_SIZE)
       handle:close()
-      return result and result:match("dark") or true
+      return isPatternMatch(result, "dark")
     end
   end
   -- Default to returning true, assuming that a light background is an exception
@@ -34,8 +42,7 @@ end
 
 return {
   -- Add themes
-  { "nordtheme/vim" },
-  { "NLKNguyen/papercolor-theme" },
+  { "nordtheme/vim", "NLKNguyen/papercolor-theme" },
 
   -- Configure LazyVim to dynamically select the colorscheme
   {
